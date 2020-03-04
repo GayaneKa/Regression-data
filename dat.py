d447eb3727c2952c4900635870845117f16c5889
GayaneKa <60619757+GayaneKa@users.noreply.github.com>
2020-03-04 04:29:16 -0600

    Add files via upload

--- dat.py
+++ dat.py
@@ -13,18 +13,15 @@ class main:
     myobject.NSigm = np.std(lst)
     myobject.Arr = lst
   def myfunc(myobject):
-    gn = np.random.normal(myobject.Mean, myobject.Sigm, (100,100))
-    return(gn)
+    gn1 = pd.DataFrame(np.random.normal(myobject.Mean, myobject.Sigm, (100,100)))
+    return(gn1)
   def myfunc2(myobject):
-    gn = np.random.normal(myobject.Mean, myobject.Sigm,100)
-    return(gn)
+    gn2 = np.random.normal(myobject.Mean, myobject.Sigm,100)
+    return(gn2)
   def newfunc(myobject):
     print('Mean =', myobject.NMean,'Sigma =',myobject.NSigm)
   def plotting(myobject):
-    #plt.hist(myobject.Arr, bins='auto')
-    #plt.hist(myobject.Arr)
-    plt.hist(myobject.Arr)
-    #plt.title("histogram") 
+    plt.hist(myobject.Arr) 
     plt.show()
     
     
@@ -33,21 +30,15 @@ def Samples(mean):
     sigma = 1
     A = main(0,1,[0,0]).myfunc()
     B = main(mu,sigma,[0,0]).myfunc2()
-    F = A.copy()
-    if mu == 0 :
-        print
-        #main(mu,sigma,A).newfunc()
-        #main(mu,sigma,A).plotting()
+    if mean == 0:
+        F = A.copy()
     else:
-        for j in range(0, 100,mean):
-            #print(j)
-            if j%mean == 0:
-                #print(j)
-                F[j-1] = B
-        E = F.copy()
-        print(F)
-        #main(mu,sigma,E).newfunc()
-        #main(mu,sigma,E).plotting()
-
-        
-Samples(0)
+        A[mean] = B
+        F = A.copy()
+    return(F)
+    '''E = F.copy()
+    main(mu,sigma,E).newfunc()
+    main(mu,sigma,E).plotting()'''
+    
+       
+print(Samples(0))
