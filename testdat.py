51bfca5ca6052ccd676e5479acc1178ca6603f63
GayaneKa <60619757+GayaneKa@users.noreply.github.com>
2020-03-06 07:08:38 -0600

    Add files via upload

---
+++ testdat.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import scipy as sp
+import numpy as np
+import dat
+
+
+def mahalanobis(x=None, data=None, cov=None):
+    """Compute the Mahalanobis Distance between each row of x and the data  
+    x    : vector or matrix of data with, say, p columns.
+    data : ndarray of the distribution from which Mahalanobis distance of each observation of x is to be computed.
+    cov  : covariance matrix (p x p) of the distribution. If None, will be computed from data.
+    """
+    #print('x',x)
+    #print('data',data)
+    x_minus_mu = x - np.mean(data)
+    if not cov:
+        cov = np.cov(data.values.T)
+    inv_covmat = np.linalg.inv(cov)
+    left_term = np.dot(x_minus_mu, inv_covmat)
+    mahal = np.dot(left_term, x_minus_mu.T)
+    return mahal.diagonal()
+
+df_x = dat.Samples(0).head(1)
+df_x['mahala'] = mahalanobis(x=df_x, data=dat.Samples(0))
+print(df_x['mahala'])
